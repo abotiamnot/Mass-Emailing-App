b9d09574d79437f4742296c238a66596efbdb6a9
abotiamnot <yatharthrai16@ducic.ac.in>
2018-06-25 17:33:52 +0530

    Edited some front end, fixed back-end, some missing features remain

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, url_for, redirect
+from flask_mail import Mail, Message
 
 import backend.core as core
 
 app = Flask(__name__)
+
+@app.route('/test', methods=['GET', 'POST'])
+def test():
+    if request.method == 'POST':
+         x = request.form['name'].upper()
+         return x
+    return render_template('test.html')
 
 @app.route('/')
 def index():
@@ -10,10 +18,35 @@ def index():
 
 @app.route('/extractmail', methods=['GET', 'POST'])
 def extractmail():
+    if request.method == 'POST':
+        message = core.email_find(request.form['file_to_extract_from'])
+        if message is True:
+            return render_template('success.html', message="Extracted. Check the file folder for the data.")
+        if message is not True:
+            return render_template('failure.html', message=message)
     return render_template('extractmail.html')
 
-@app.route('/sendmail')
+@app.route('/sendmail', methods=['GET', 'POST'])
 def sendmail():
+    if request.method == 'POST':
+        try:
+            app.config.update(
+            DEBUG=True,
+            MAIL_SERVER='smtp.gmail.com',
+            MAIL_PORT=465,
+            MAIL_USE_SSL=True,
+            MAIL_USERNAME = request.form['senderemail'],
+            MAIL_PASSWORD = request.form['senderpassword']
+            )
+            mail = Mail(app)
+            msg = Message(request.form['emailsubject'],
+            sender=request.form['senderemail'],
+    		recipients=request.form['recipientemail'].split())
+            msg.body = request.form['emailcontent']
+            mail.send(msg)
+            return render_template('success.html', message="Email Sent!")
+        except Exception as e:
+            return render_template('failure.html', message=e)
     return render_template('sendmail.html')
 
 @app.route('/about')
--- backend/core.py
+++ backend/core.py
@@ -1,22 +1,27 @@
 import re
 import csv
-import smtplib
+from flask_mail import Mail, Message
+from datetime import datetime
 
-def email_find(name, extract_name=None, csv_write=True):
-    if extract_name is None:
-        extract_name = "Email_List.csv"
-    else:
-        extract_name = extract_name + '.csv'
+def email_find(name_is):
     email_regex = re.compile(r'.+@\S+')
-    f = open(name,"r")
-    contents = f.read()
-    f.close()
-    email_list = email_regex.findall(contents)
-    with open(extract_name, "w") as csv_file:
-            writer = csv.writer(csv_file)
-            for email in email_list:
-                writer.writerow([email])
+    try:
+        if name_is[-3:] is not '.csv':
+            raise Exception("That's not a .TXT file")
+        f = open(name_is,"r")
+        contents = f.read()
+        f.close()
+        email_list = email_regex.findall(contents)
+        current_name = datetime.datetime.now().strftime("%Y-%m-%d %H-%M-%S") + '.csv'
+        with open(current_name, "w") as csv_file:
+                writer = csv.writer(csv_file)
+                for email in email_list:
+                    writer.writerow([email])
+        return True
+    except Exception as e:
+        return e
 
+# Won't be used since this is already being done using flask_mail
 def send_email(user=None, passw=None, msg=None, sendto=None):
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
