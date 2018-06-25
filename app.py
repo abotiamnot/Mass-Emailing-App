0480e9dd190300f171ee017e2887eff653eac563
abotiamnot <yatharthrai16@ducic.ac.in>
2018-06-25 12:29:40 +0530

    Front-end done

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
 import backend.core as core
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/extractmail')
+@app.route('/extractmail', methods=['GET', 'POST'])
 def extractmail():
-    return render_template('index.html')
+    return render_template('extractmail.html')
 
 @app.route('/sendmail')
 def sendmail():
-    return render_template('index.html')
+    return render_template('sendmail.html')
 
 @app.route('/about')
 def about():
