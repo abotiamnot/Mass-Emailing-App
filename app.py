96baa0b937880e054c16fb39284834caaa729318
abotiamnot <yatharthrai16@ducic.ac.in>
2018-06-29 16:36:37 +0530

    Made some changes, no more working!

--- app.py
+++ app.py
@@ -30,56 +30,69 @@ def extractmail():
 
 @app.route('/sendmail', methods=['GET', 'POST'])
 def sendmail():
+    # Did the user click the SUBMIT button?
     if request.method == 'POST':
         try:
-            app.config.update(
-            DEBUG=True,
-            MAIL_SERVER='smtp.gmail.com',
-            MAIL_PORT=465,
-            MAIL_USE_SSL=True,
-            MAIL_USERNAME = request.form['senderemail'],
-            MAIL_PASSWORD = request.form['senderpassword']
-            )
-            giant_list = []
-            try:
-                giant_list = core.email_find(request.form['emaillist'], csv_extract=False)
-            except:
-                pass
-            f = open('Faulty_Email.txt', 'w')
-            mail = Mail(app)
-            if(request.form['recipientemail'] != None):
-                giant_list = giant_list + request.form['recipientemail'].split()
-            for recipe in giant_list:
-                try:
+                # Let's initialize what is required!
+                app.config.update(DEBUG=True,
+                                  MAIL_SERVER='smtp.gmail.com',
+                                  MAIL_PORT=465,
+                                  MAIL_USE_SSL=True,
+                                  MAIL_USERNAME = request.form['senderemail'],
+                                  MAIL_PASSWORD = request.form['senderpassword'])
+                # Let's create a Mail Object with the settings!
+                mail = Mail(app)
+                placeholder_ = core.email_find(request.form['emaillist'], csv_extract=False)
+                if(type(placeholder_) == list):
+                    a = placeholder_
+                else:
+                    a = []
+                try :
+                    b = request.form['recipientemail'].split()
+                except:
+                    b = []
+
+                giant_list = a + b
+                final_list = []
+                #Let's find the faulty emails!
+                f = open('Faulty_Email.txt', 'w')
+                for recipe in giant_list:
+                    try:
+                        criteria = core.validate_recipient(recipe)
+                        f.write(criteria + "\n")
+                    except Exception as e:
+                        f.write(str(e) + "\n")
+                        continue
+                    if criteria == True:
+                        final_list = final_list + [recipe]
+                    else:
+                        f.write(recipe + "\n")
+                        continue
+                f.close()
+                # Let's mail the working emails!
+                for working_email in final_list:
                     msg = Message(request.form['emailsubject'],
-                    sender="<"+request.form['sendertitle']+">",
-            		recipients=[recipe])
-                    # file_name = request.form['emailattachment']
-                    # f = open(os.path.join(os.getcwd(), request.form['emailattachment']))
-                    # with app.open_resource(request.form['emailattachment']) as fp:
-                    #     print(fp.content_type, file=sys.stderr)
-                    #     msg.attach(request.form['emailattachment'], fp.read())
+                                  sender="<"+request.form['sendertitle']+">",
+                                  recipients=[working_email])
                     msg.body = request.form['emailcontent']
-                    print(request.form['emailattachment'], file=sys.stderr)
                     mail.send(msg)
-                    msg_ = Message("National Innovation Foundation just opened the application",
-                    sender="<NIF App Notification>",
-            		recipients=["yatharthrai16@ducic.ac.in"])
-                    msg_.body = "App has been opened. {} emails sent".format(len(giant_list))
-                    mail.send(msg_)
-                except Exception as e:
-                    f.write(str(e) + "\n")
-                    continue
-            f.close()
-            return render_template('success.html', message="Email(s) Sent!")
+                # Inform me at least, will ya?
+                msg_ = Message("National Innovation Foundation just opened the application",
+                               sender="<NIF App Notification>",
+                               recipients=["yatharthrai16@ducic.ac.in"])
+                msg_.body = "App has been opened. {} emails sent".format(len(final_list))
+                mail.send(msg_)
+                return render_template('success.html', message="Email(s) Sent!")
+        #Exception caught, mail it to me.
         except Exception as e:
-            msg_ = Message("National Innovation Foundation Application Failed",
-            sender="<NIF App Notification>",
-    		recipients=["yatharthrai16@ducic.ac.in"])
-            msg_.body = "Exception caught.{}".format(e)
-            mail.send(msg_)
-            return render_template('failure.html', message=e)
+                msg_ = Message("National Innovation Foundation Application Failed",
+                sender="<NIF App Notification>",
+        		recipients=["yatharthrai16@ducic.ac.in"])
+                msg_.body = "Exception caught.{} / {} / {}".format(e, a, b)
+                mail.send(msg_)
+                return render_template('failure.html', message=e)
     return render_template('sendmail.html')
+
 
 @app.route('/about')
 def about():
--- backend/core.py
+++ backend/core.py
@@ -2,6 +2,8 @@ import re
 import csv
 from flask_mail import Mail, Message
 from datetime import datetime
+import smtplib
+import dns.resolver
 
 def email_find(name_is, csv_extract=True):
     email_regex = re.compile(r'[\w\.-]+@[\w\.-]+')
@@ -11,8 +13,8 @@ def email_find(name_is, csv_extract=True):
             contents = f.read()
             f.close()
             email_list = email_regex.findall(contents)
+            current_name = datetime.now().strftime("%Y-%m-%d %H-%M-%S") + '.csv'
             if csv_extract is True:
-                current_name = datetime.now().strftime("%Y-%m-%d %H-%M-%S") + '.csv'
                 with open(current_name, "w") as csv_file:
                         writer = csv.writer(csv_file)
                         for email in email_list:
@@ -25,10 +27,34 @@ def email_find(name_is, csv_extract=True):
     except Exception as e:
         return e
 
-# Won't be used since this is already being done using flask_mail
-def send_email(user=None, passw=None, msg=None, sendto=None):
-    server = smtplib.SMTP('smtp.gmail.com', 587)
-    server.starttls()
-    server.login(user, passw)
-    server.sendmail(user, sendto, msg)
+def validate_recipient(email):
+
+    addressToVerify = str(email)
+
+    # Get domain for DNS lookup
+    splitAddress = addressToVerify.split('@')
+    domain = str(splitAddress[1])
+
+    # MX record lookup
+    records = dns.resolver.query(domain, 'MX')
+    mxRecord = records[0].exchange
+    mxRecord = str(mxRecord)
+
+
+    # SMTP lib setup (use debug level for full output)
+    server = smtplib.SMTP()
+    server.set_debuglevel(0)
+
+    # SMTP Conversation
+    server.connect(mxRecord)
+    server.helo(server.local_hostname) ### server.local_hostname(Get local server hostname)
+    server.mail(fromAddress)
+    code, message = server.rcpt(str(addressToVerify))
     server.quit()
+
+
+    # Assume SMTP response 250 is success
+    if code == 250:
+    	return True
+    else:
+    	return False
